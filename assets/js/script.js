132fc8aa21f639713dd67783bba2b039ee09bf07
korzair <ariquelme.snchz@gmail.com>
2024-06-16 05:20:26 -0400

    se agregan estilos de pagina 2, y js en pagina

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,35 @@
-const img1 =document.getElementById("img1")
+const imagenOne = document.getElementById("img1")
 
-        img1.addEventListener("click", function()
+        imagenOne.addEventListener("click", function()
     {
         if (img1.style.border === "2px solid red")
         {
-            image.style.border = "";
+            img1.style.border = "";
 
         }
         else {
-            image.style.border = "2px solid red";
+            img1.style.border = "2px solid red";
         }
     });
+
+    // document.getElementById('calcular').addEventListener('click', function() {
+        
+    //     const sticker1 = parseInt(document.getElementById('sticker1').value) || 0;
+    //     const sticker2 = parseInt(document.getElementById('sticker2').value) || 0;
+    //     const sticker3 = parseInt(document.getElementById('sticker3').value) || 0;
+
+    //     // Calcular suma de los stickers
+    //     const total = sticker1 + sticker2 + sticker3;
+
+    //     // Referencia párrafo de resultado
+    //     const resultado = document.getElementById('resultado');
+
+    //     // Verificar si el total es menor o igual a 10
+    //     if (total <= 10) {
+    //         resultado.textContent = `Has seleccionado ${total} stickers.`;
+    //         resultado.style.color = 'green';
+    //     } else {
+    //         resultado.textContent = `Has seleccionado demasiados stickers. Máximo permitido es 10.`;
+    //         resultado.style.color = 'red';
+    //     }
+    // });
