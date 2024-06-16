3f2f28c5984049e5d6459133ec29075acda1cae9
korzair <ariquelme.snchz@gmail.com>
2024-06-15 22:06:14 -0400

    se agregan assets y paginas ademas de completar js primera pagina

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+const img1 =document.getElementById("img1")
+
+        img1.addEventListener("click", function()
+    {
+        if (img1.style.border === "2px solid red")
+        {
+            image.style.border = "";
+
+        }
+        else {
+            image.style.border = "2px solid red";
+        }
+    });
